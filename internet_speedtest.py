71d6a766c578d0cdc8e98cf847d56e976cd72e79
Derek21298 <Derek21298@gmail.com>
2019-06-10 22:15:12 -0400

    internet speed test script

---
+++ internet_speedtest.py
@@ -0,0 +1,56 @@
+# internet_speedtest.py
+# 
+# Author: Derek Haas
+# 
+# Description: Use speedtest-cli to collect information about download,
+# speed, upload speed, and ping
+#
+# Date: June 10 2019
+#
+
+import speedtest
+
+# Function to conduct the speetest
+def test(): 
+
+	# Get the best server and get the results
+	s = speedtest.Speedtest()
+	s.get_servers()
+	s.get_best_server()
+	s.download()
+	s.upload()
+	
+	result = s.results.dict()
+
+	# Results are a dictionary
+	return result["download"], result["upload"], result["ping"]
+
+def main():
+
+	# Get the download, upload, and ping from the test
+	download, upload, ping = test()
+
+	# convert to Mbps
+	download = download * 1e-6
+	upload = upload * 1e-6
+	
+	# Write to a csv
+	with open('log.csv', 'w') as file:
+		file.write('Download \t Upload \t Ping\n')
+		file.write('%.3f'%download);
+		file.write(' Mbps\t ')
+		file.write(str('%.3f'%upload))
+		file.write(' Mbps\t ')
+		file.write(str(ping))
+		file.write(' ms\n')
+
+		file.close()
+
+if __name__ == '__main__':
+	main()	
+
+
+
+
+
+
