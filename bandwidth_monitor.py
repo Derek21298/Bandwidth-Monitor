cf5bb367b2b975af9714ee7314562d7e830d1723
Derek21298 <Derek21298@gmail.com>
2019-06-12 21:10:34 -0400

    Fixed print format and header.  Doesnt seem to report correct size

--- bandwidth_monitor.py
+++ bandwidth_monitor.py
@@ -1,20 +1,28 @@
-#Sniffs only incoming TCP packet
+# bandwidth_monitor.py
+# 
+# Author: Derek Haas
+# Description: Sniff incoming and outgoing packets to determine the data size
+#
+# Date: June 12 2019
+#
 
 import socket, sys
 from struct import *
 import sys
 import time
 
-#create an INET, STREAMing socket
+# Create an INET, STREAMing socket
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 except socket.error or msg:
 	print('Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
 	sys.exit()
 
+# Time is epoch time (January 1 1970)
 dataTotal = 0
 startTime = time.time()
 
+# Always get packets
 while True:
 
 	# Get a packet
@@ -49,16 +57,30 @@ while True:
 
 	dataTotal = dataTotal + sys.getsizeof(data)
 
+	# If 60 seconds has passed, update the data
 	currTime = time.time()
-	
-#	if(currTime > (startTime + 5)):
-#		print('5 Seconds have passed') 
-
-
 	if(currTime > (startTime + 60)): 
 			
 		print('1 Minute has gone by!!!')
-		print('Data size: ' + str(dataTotal) + ' bytes')
+
+		# If the data sent is between 1k and 1M print as 1 kB
+		if((dataTotal > 1000) and (dataTotal < 1000000)):
+			dataTotal = dataTotal * 1e-3
+			print('Data sent in the past minute: %.2f KB'%dataTotal)
+		
+		# If the data sent is between 1M and 1G print as 1M
+		elif((dataTotal > 1000000) and (dataTotal < 1000000000)):
+			dataTotal = dataTotal * 1e-6
+			print('Data sent in the past minute: %.2f MB'%dataTotal)
+
+		# If the data sent is between 1G and 1T print as 1G
+		elif((dataTotal > 1000000000) and (dataTotal < 1000000000000)):
+			dataTotal = dataTotal * 1e-9
+			print('Data sent in the past minute: %.2f GB'%dataTotal)
+
+		# Else just print as bytes
+		else:
+			print('Data sent in the past minute: %d B'%dataTotal)
+
 		dataTotal = 0
 		startTime = time.time()
-	
