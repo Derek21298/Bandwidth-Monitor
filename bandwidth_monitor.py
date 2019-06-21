71d271b2070808c79054749eec00091e35746f41
Derek21298 <Derek21298@gmail.com>
2019-06-20 21:21:33 -0400

    New method of gathering data with putils.  Reports correct data usage

--- bandwidth_monitor.py
+++ bandwidth_monitor.py
@@ -1,105 +1,87 @@
 # bandwidth_monitor.py
 # 
 # Author: Derek Haas
-# Description: Sniff incoming and outgoing packets to determine the data size
+# Description: Use psutil to get the net io counters to report data usage
 #
 # Date: June 12 2019
 #
 
-import socket, sys
-from struct import *
+import psutil
 import time
 
+print('Getting preliminary data...')
 
-#define ETH_P_ALL 0x0003 <-- every packet
+# Get the all the network stats for the first calculation
+net_stats = psutil.net_io_counters(pernic=True)
 
-# Create an INET, STREAMing socket
-try:
-	s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
-except socket.error:
-	print('Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-	sys.exit()
+# Print the net stats (mainly for debugging)
+'''
+print('Network Interface: lo\t\tBytes Sent: %d'%net_stats['lo'][1])
+print('Network Interface: lo\t\tBytes Received: %d\n'%net_stats['lo'][2])
+print('Network Interface: enp4s0\tBytes Sent: %d'%net_stats['enp4s0'][1])
+print('Network Interface: enp4s0\tBytes Received: %d\n'%net_stats['enp4s0'][2])
+print('Network Interface: wlp5s0\tBytes Sent: %d'%net_stats['wlp5s0'][1])
+print('Network Interface: wlp5s0\tBytes Received: %d\n'%net_stats['wlp5s0'][2])
+'''
 
-# Time is epoch time (January 1 1970)
-dataTotal = 0
-data = 0
+# Get the bytes sent and received for the different interfaces
+lo_sent_start = net_stats['lo'][1]
+lo_received_start = net_stats['lo'][2]
+enp_sent_start = net_stats['enp4s0'][1]
+enp_received_start = net_stats['enp4s0'][2]
+wlp_sent_start = net_stats['wlp5s0'][1]
+wlp_received_start = net_stats['wlp5s0'][2]
+
+
+# Begin to get data every minute
+print('Starting to collect data...')
+
 startTime = time.time()
-
-# Always get packets
+periodTime = time.time()
 while True:
 
-	# Get a packet
-	packet = s.recvfrom(65565)
-	
-	# Packet string from tuple
-	packet = packet[0]
+	currTime = time.time()
 
-	# Length of ethernet header
-	eth_length = 14
-
-	# Take first 20 characters for ip header
-	ip_header = packet[0:20]
-	
-	# Unpack the header and get length
-	iph = unpack('!BBHHHBBH4s4s' , ip_header)
-	version_ihl = iph[0]
-	ihl = version_ihl & 4
-	iph_length = ihl * 4
-	protocol = iph[6]
-
-	# TCP packets
-	if(protocol == 6):
-	 
-		# Get tcp header right after io header
-		tcp_header = packet[iph_length:iph_length+20]
+	# If 2 seconds pass, print a '.' as a status bar
+	if(currTime > (periodTime + 2)):
+		print('.', end='', flush=True)
+		periodTime = time.time()
 		
-		# Unpack header and get length
-		tcph = unpack('!HHLLBBHHH' , tcp_header)
-		doff_reserved = tcph[4]
-		tcph_length = doff_reserved >> 4
-		
-		# Size of the data is the packet size without the headers
-		h_size = iph_length + tcph_length * 4
-		data_size = len(packet) - h_size
-		
-		# Get the actual data
-		data = packet[h_size:]
-
-	# ICMP packets
-	elif(protocol == 1):
-	
-		# Length of ICMP header
-		icmph_length = 4
-		
-		# Get the data
-		h_size = eth_length + iph_length + icmph_length
-		data_size = len(packet) - h_size
-		data = packet[h_size:]	
-		
-	# UDP packets
-	elif(protocol == 17):
-
-		# UDP header length
-		udph_length = 8
-
-		# Get the data
-		h_size = eth_length + iph_length + udph_length
-		data_size = len(packet) - h_size
-		data = packet[h_size:]	
-
-	# If not one of the 3 protocols
-	else:
-		print('Protocol other than TCP/ICMP/UDP')
-			
-	# Keep track of the running total
-	dataTotal = dataTotal + sys.getsizeof(data)
 
 	# If 60 seconds has passed, print the total data and reset
-	currTime = time.time()
-	if(currTime > (startTime + 60)): 
-			
-		print('1 Minute has gone by!!!')
+	if(currTime > (startTime + 60)):
+		 
+		print('\n')	
+		print('1 Minute has gone by!!!\n')
 
+		# Get the current data
+		net_stats = psutil.net_io_counters(pernic=True)
+		
+		lo_sent_curr = net_stats['lo'][1]
+		lo_received_curr = net_stats['lo'][2]
+		enp_sent_curr = net_stats['enp4s0'][1]
+		enp_received_curr = net_stats['enp4s0'][2]
+		wlp_sent_curr = net_stats['wlp5s0'][1]
+		wlp_received_curr = net_stats['wlp5s0'][2]
+
+		lo_sent = lo_sent_curr - lo_sent_start
+		enp_sent = enp_sent_curr - enp_sent_start
+		wlp_sent = wlp_sent_curr - wlp_sent_start
+
+		# (Current - Start) is the data sent over a minute
+		lo_received = lo_received_curr - lo_received_start
+		enp_received = enp_received_curr - enp_received_start
+		wlp_received = wlp_received_curr - wlp_received_start
+
+		print('Network Interface: lo\t\tBytes Sent: %d'%lo_sent)
+		print('Network Interface: lo\t\tBytes Received: %d\n'%lo_received)
+		print('Network Interface: enp4s0\tBytes Sent: %d'%enp_sent)
+		print('Network Interface: enp4s0\tBytes Received: %d\n'%enp_received)
+		print('Network Interface: wlp5s0\tBytes Sent: %d'%wlp_sent)
+		print('Network Interface: wlp5s0\tBytes Received: %d\n'%wlp_received)
+
+		dataTotal = lo_sent + enp_sent + wlp_sent
+		
 		# If the data sent is between 1k and 1M print as 1 kB
 		if((dataTotal > 1000) and (dataTotal < 1000000)):
 			dataTotal = dataTotal * 1e-3
@@ -119,5 +101,19 @@ while True:
 		else:
 			print('Data sent in the past minute: %d B'%dataTotal)
 
+		# Reset all the variables
 		dataTotal = 0
 		startTime = time.time()
+		periodTime = time.time()
+		
+		lo_sent_start = lo_sent_curr
+		lo_received_start = lo_received_curr
+		enp_sent_start = enp_sent_curr
+		enp_received_start = enp_received_curr
+		wlp_sent_start = wlp_sent_curr
+		wllp_received_start = wlp_received_curr
+
+
+
+
+
